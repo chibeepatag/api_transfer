0051325284b8d071059fb089f3f66fba0c0d87f5
Christian Eric O. Cosca <christiancosca@gmail.com>
2022-04-25 22:22:50 +0800

    Having issues for controller naming

--- app/controllers/data_transfer_controller.rb
+++ app/controllers/data_transfer_controller.rb
@@ -1,17 +1,17 @@
 class DataTransferController < ApplicationController
     def index
-        @dataTransfer = DataTransfer.all
+        @dt = DataTransfer.all
     end
 
     def new
-        @dataTransfer = DataTransfer.new
+        @dt = DataTransfer.new
     end
 
     def create
-        @dataTransfer = DataTransfer.new(params[:data_transfer])
-        if @dataTransfer.save
+        @dt = DataTransfer.new(params[:data_transfer])
+        if @dt.save
           flash[:success] = "Object successfully created"
-          redirect_to @dataTransfer
+          redirect_to @dt
         else
           flash[:error] = "Something went wrong"
           render 'new'
@@ -19,12 +19,13 @@ class DataTransferController < ApplicationController
     end
     
     def show
-        @dataTransfer = DataTransfer.find(params[:id])
+        @dt = DataTransfer.find(params[:id])
     end
     
     private 
         def data_transfer_params 
             params.require(:data_transfer).permit(:c4_acct_name, :c4_token,
-            )
+                :c3_acct_name, :c3_token, :c3_url, :page,
+                :job_start_time, :job_end_time, :errors)
         end
 end
