dded6c6f66fda05d863bd5887bd71ec2ff965913
Christian Eric O. Cosca <christiancosca@gmail.com>
2022-04-25 22:05:36 +0800

    Created a DataTransferController actions: new, index, create, show AND modified the migragion by adding c3_token

--- app/controllers/data_transfer_controller.rb
+++ app/controllers/data_transfer_controller.rb
@@ -1,2 +1,30 @@
 class DataTransferController < ApplicationController
+    def index
+        @dataTransfer = DataTransfer.all
+    end
+
+    def new
+        @dataTransfer = DataTransfer.new
+    end
+
+    def create
+        @dataTransfer = DataTransfer.new(params[:data_transfer])
+        if @dataTransfer.save
+          flash[:success] = "Object successfully created"
+          redirect_to @dataTransfer
+        else
+          flash[:error] = "Something went wrong"
+          render 'new'
+        end
+    end
+    
+    def show
+        @dataTransfer = DataTransfer.find(params[:id])
+    end
+    
+    private 
+        def data_transfer_params 
+            params.require(:data_transfer).permit(:c4_acct_name, :c4_token,
+            )
+        end
 end
--- db/migrate/20220425133736_create_data_transfers.rb
+++ db/migrate/20220425133736_create_data_transfers.rb
@@ -4,6 +4,7 @@ class CreateDataTransfers < ActiveRecord::Migration[7.0]
       t.string :c4_acct_name
       t.string :c4_token
       t.string :c3_acct_name
+      t.string :c3_token
       t.string :c3_url
       t.integer :page
       t.timestamp :job_start_time
