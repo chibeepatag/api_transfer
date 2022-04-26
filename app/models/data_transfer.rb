cd299083e81c2d2534b4edc2ca4581e1556333a7
Christian Eric O. Cosca <christiancosca@gmail.com>
2022-04-26 08:40:12 +0800

    z

--- app/models/data_transfer.rb
+++
@@ -1,2 +0,0 @@
-class DataTransfer < ApplicationRecord
-end
--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  resources :data_transfers
-  root "data_transfers#new"
-end
