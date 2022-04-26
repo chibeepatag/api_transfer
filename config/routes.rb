f94d10ee64a8efccf3727011f06d7989d1763c4c
Christian Eric O. Cosca <christiancosca@gmail.com>
2022-04-26 11:32:29 +0800

    redo migration

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root "data_transfers#new"
+  resources :data_transfers
+end
