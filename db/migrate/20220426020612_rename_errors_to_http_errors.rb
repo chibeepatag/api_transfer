e4be2b7f1c92acc3c160ce14db9d9ef9c89f3f35
Christian Eric O. Cosca <christiancosca@gmail.com>
2022-04-26 11:31:30 +0800

    x

--- db/migrate/20220426020612_rename_errors_to_http_errors.rb
+++
@@ -1,7 +0,0 @@
-class RenameErrorsToHttpErrors < ActiveRecord::Migration[7.0]
-  def change
-    change_table :data_transfers do |t|
-      t.rename :errors, :http_errors
-    end
-  end
-end
--- db/migrate/20220426032515_add_attribute_to_model.rb
+++
@@ -1,5 +0,0 @@
-class AddAttributeToModel < ActiveRecord::Migration[7.0]
-  def change
-    add_column :models, :transfer_data_type, :string
-  end
-end
